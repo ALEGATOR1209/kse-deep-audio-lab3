33b18d4c8e45b31bdd92fe76d174987154fa10f8
NeProgramist <zaskozhenya@gmail.com>
2026-06-30 00:31:13 +0300

    load data

---
+++ load_data.sh
@@ -0,0 +1,6 @@
+# curl -L "https://drive.usercontent.google.com/download?id=1j9d91QqE7_WnOnmEmidtOG55tpmxQUeJ&export=download&confirm=t" -o data/toronto.zip
+# unzip data/toronto.zip -d data/toronto
+
+curl -L "https://www.kaggle.com/api/v1/datasets/download/mfekadu/darpa-timit-acousticphonetic-continuous-speech" -o data/timit.zip
+unzip data/timit.zip -d data/timit
+ 
